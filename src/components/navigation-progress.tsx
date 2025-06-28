b61b07a0cb2e1263e772a1ea9dc71a614e8b48e2
Lhacen Med <2l7acenmed653@gmail.com>
2025-06-28 23:51:00 +0000

    Update navigation progress styles for improved visibility

--- src/components/navigation-progress.tsx
+++ src/components/navigation-progress.tsx
@@ -10,7 +10,7 @@ const styles = `
     pointer-events: none;
   }
   #nprogress .bar {
-    background: white;
+    background: #111827;
     position: fixed;
     z-index: 1031;
     top: 0;
@@ -24,7 +24,7 @@ const styles = `
     right: 0px;
     width: 100px;
     height: 100%;
-    box-shadow: 0 0 10px white, 0 0 5px white;
+    box-shadow: 0 0 10px #111827, 0 0 5px #111827;
     opacity: 1.0;
     transform: rotate(3deg) translate(0px, -4px);
   }
